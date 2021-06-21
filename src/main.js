9a57a0f9d440c31a98172cb3cb889e877450edaa
ham-666 <18709244518@163.com>
2021-06-21 20:06:15 +0800

    增加插件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugins/element.js'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
